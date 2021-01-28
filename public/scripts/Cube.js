64e4283b3fd0ad2429ce8fe3bc14c9f4a43b197a
zhenya750 <zhenya750bus@mail.ru>
2021-01-28 23:03:50 +0300

    divided into classes

---
+++ public/scripts/Cube.js
@@ -0,0 +1,423 @@
+import * as THREE from './three_data/build/three.module.js';
+
+export class Cube {
+
+    WHITE  = 0xffffff;
+    ORANGE = 0xf4844c;
+    RED    = 0xe14343;
+    YELLOW = 0xebe457;
+    BLUE   = 0x3a49ee;
+    GREEN  = 0x37cd2f;
+    BLACK  = 0x000000;
+
+    constructor(n) {
+        this.N = n;
+        this.ThreeObject = new THREE.Group();
+        this.#setCubeFaces();
+
+        this.Rotator = new THREE.Group();
+        this.ThreeObject.add(this.Rotator);
+        this.ThreeObject.add(new THREE.AxesHelper(3));
+
+        this.StateMap = {
+            U : this.ThreeObject.children.slice(0,                   this.N * this.N),
+            L : this.ThreeObject.children.slice(this.N * this.N,     this.N * this.N * 2),
+            B : this.ThreeObject.children.slice(this.N * this.N * 2, this.N * this.N * 3),
+            R : this.ThreeObject.children.slice(this.N * this.N * 3, this.N * this.N * 4),
+            F : this.ThreeObject.children.slice(this.N * this.N * 4, this.N * this.N * 5),
+            D : this.ThreeObject.children.slice(this.N * this.N * 5, this.N * this.N * 6)
+        };
+
+        this.PlanesBetweenLayers = {
+            F : this.#createPlanesBetweenLayers('F'),
+            R : this.#createPlanesBetweenLayers('R'),
+            U : this.#createPlanesBetweenLayers('U')
+        };
+
+        this.#setPlanesBetweenLayers();
+        
+        this.KERNEL = this.#createKernel();
+
+        this.LastRotatedLayer = {
+            face  : 'F',    // can be any face
+            index : 0,
+            angle : 0
+        }
+    }
+
+
+    // TODO: - create a certain layer for planes only (for raycast)
+    #createFace(color, backgroundColor) {
+        const group = new THREE.Group();
+        const planeSize = 1;
+        const padding = planeSize / 7;
+
+        const k = (this.N - 1) / 2;
+
+        const backgroundGeometry = new THREE.PlaneGeometry(planeSize, planeSize);
+        const backgroundMaterial = new THREE.MeshPhongMaterial({
+            color: backgroundColor,
+            side: THREE.FrontSide,
+            polygonOffset: true,
+            polygonOffsetFactor: 2
+        });
+
+        const foregroundGeometry = new THREE.PlaneGeometry(planeSize - padding, planeSize - padding);
+        const foregroundMaterial = new THREE.MeshPhongMaterial({
+            color: color,
+            side: THREE.FrontSide,
+            polygonOffset: true,
+            polygonOffsetFactor: 1.8
+        });
+
+        for (let y = k; y >= -k; y--) {
+            for (let x = -k; x <= k; x++) {
+                const backgroundPlane = new THREE.Mesh(backgroundGeometry, backgroundMaterial);
+                const foregroundPlane = new THREE.Mesh(foregroundGeometry, foregroundMaterial);
+                backgroundPlane.add(foregroundPlane);
+
+                backgroundPlane.position.x = x * planeSize;
+                backgroundPlane.position.y = y * planeSize;
+
+                group.add(backgroundPlane);
+            }
+        }
+
+        return group;
+    }
+
+
+    #setCubeFaces() {
+        const d = this.N / 2;
+        const up = this.#createFace(this.YELLOW, this.BLACK);
+        up.rotateX(-Math.PI / 2);
+        up.position.y = d;
+        while (up.children.length > 0) this.ThreeObject.attach(up.children[0]);
+        
+        const left = this.#createFace(this.ORANGE, this.BLACK);
+        left.rotateY(-Math.PI / 2);
+        left.position.x = -d;
+        while (left.children.length > 0) this.ThreeObject.attach(left.children[0]);
+        
+        const back = this.#createFace(this.GREEN, this.BLACK);
+        back.rotateY(Math.PI);
+        back.position.z = -d;
+        while (back.children.length > 0) this.ThreeObject.attach(back.children[0]);
+        
+        const right = this.#createFace(this.RED, this.BLACK);
+        right.rotateY(Math.PI / 2);
+        right.position.x = d;
+        while (right.children.length > 0) this.ThreeObject.attach(right.children[0]);
+        
+        const front = this.#createFace(this.BLUE, this.BLACK);
+        front.position.z = d;
+        while (front.children.length > 0) this.ThreeObject.attach(front.children[0]);
+        
+        const down = this.#createFace(this.WHITE, this.BLACK);
+        down.rotateX(Math.PI / 2);
+        down.position.y = -d;
+        while (down.children.length > 0) this.ThreeObject.attach(down.children[0]);
+    }
+
+
+    #createPlanesBetweenLayers(face) {
+        const planeSize = 1 * this.N;
+        let planesBetweenLayers = [];
+    
+        const geometry = new THREE.PlaneGeometry(planeSize, planeSize);
+        const material = new THREE.MeshPhongMaterial({
+            color: 0xff0000,
+            side: THREE.FrontSide,
+            polygonOffset: true,
+            polygonOffsetFactor: 3,
+        });
+    
+        let offset = this.N / 2;
+        
+        let axis = '';
+        if (face === 'F') axis = 'z'
+        if (face === 'R') axis = 'x';
+        if (face === 'U') axis = 'y';
+        
+        for (let index = 0; index < this.N; index++) {
+            let plane1 = null;
+            if (Math.abs(offset) < this.N / 2) {
+                plane1 = new THREE.Mesh(geometry, material);
+                plane1.position[axis] = offset;
+    
+                if (face === 'U') plane1.rotateX(-Math.PI / 2);
+                if (face === 'R') plane1.rotateY(Math.PI / 2);
+            }
+            
+            offset--;
+            
+            let plane2 = null;
+            if (Math.abs(offset) < this.N / 2) {
+                plane2 = new THREE.Mesh(geometry, material);
+                plane2.position[axis] = offset;
+    
+                if (face === 'U') plane2.rotateX(-Math.PI / 2);
+                if (face === 'R') plane2.rotateY(Math.PI / 2);
+    
+                plane2.rotateY(Math.PI);
+            }
+    
+            if (plane1) {
+                if (plane2) plane1.attach(plane2);
+                planesBetweenLayers.push(plane1);
+            }
+            else {
+                planesBetweenLayers.push(plane2);
+            }
+        }
+    
+        return planesBetweenLayers;
+    };
+
+
+    #setPlanesBetweenLayers() {
+        for (let key in this.PlanesBetweenLayers) {
+            this.PlanesBetweenLayers[key].forEach(plane => {
+                this.ThreeObject.attach(plane);
+                plane.visible = false;
+            });
+        }
+    }
+
+
+    #showPlanesAroundLayer(face, index) {
+        for (let i = -1; i <= 1; i++) {
+            if (index + i >= 0 && index + i < this.N) {
+                this.PlanesBetweenLayers[face][index + i].visible = true;
+            }
+        }
+    }
+    
+
+    #hidePlanesAroundLayer(face, index) {
+        for (let i = -1; i <= 1; i++) {
+            if (index + i >= 0 && index + i < this.N) {
+                this.PlanesBetweenLayers[face][index + i].visible = false;
+            }
+        }
+    }
+
+
+    #createKernel() {
+        const N = this.N;
+        return {
+            F : {
+                R : (i) => { return { index : i,               step : N } },
+                U : (i) => { return { index : N * (N - i - 1), step : 1 } },
+                L : (i) => { return { index : N * N - i - 1,   step : -N } },
+                D : (i) => { return { index : (i + 1) * N - 1, step : -1 } },
+                axis : 'z',
+                adjacentFaces : ['R', 'U', 'L', 'D'],
+                oppositeFace : 'B',
+                layerRotations : Array.from({ length: N }, () => this.Rotator.rotation.toVector3()),
+                layerPreviousAngles : new Array(N).fill(0)
+            },
+        
+            R : {
+                B : (i) => { return { index : N * (N - 1) + i, step : -N } },
+                U : (i) => { return { index : N - i - 1,       step : N } },
+                F : (i) => { return { index : N - i - 1,       step : N } },
+                D : (i) => { return { index : N - i - 1,       step : N } },
+                axis : 'x',
+                adjacentFaces : ['B', 'U', 'F', 'D'],
+                oppositeFace : 'L',
+                layerRotations : Array.from({ length: N }, () => this.Rotator.rotation.toVector3()),
+                layerPreviousAngles : new Array(N).fill(0)
+            },
+        
+            U : {
+                R : (i) => { return { index : N * i, step : 1 } },
+                B : (i) => { return { index : N * i, step : 1 } },
+                L : (i) => { return { index : N * i, step : 1 } },
+                F : (i) => { return { index : N * i, step : 1 } },
+                axis : 'y',
+                adjacentFaces : ['R', 'B', 'L', 'F'],
+                oppositeFace : 'D',
+                layerRotations : Array.from({ length: N }, () => this.Rotator.rotation.toVector3()),
+                layerPreviousAngles : new Array(N).fill(0)
+            }
+        };
+    }
+
+
+    rotateLayer(face, index, deg) {
+        const layerRotation = this.KERNEL[face].layerRotations[index];
+    
+        this.Rotator.rotation.x = layerRotation.x;
+        this.Rotator.rotation.y = layerRotation.y;
+        this.Rotator.rotation.z = layerRotation.z;
+    
+        if (this.Rotator.children.length === 0) {
+            if (index === 0) {
+                this.StateMap[face].forEach(plane => this.Rotator.attach(plane));
+            }
+            else if (index === this.N - 1) {
+                this.StateMap[this.KERNEL[face].oppositeFace].forEach(plane => this.Rotator.attach(plane));
+            }
+    
+            this.KERNEL[face].adjacentFaces.forEach(adj => {
+    
+                let iterator = this.KERNEL[face][adj](index);
+    
+                for (let i = 0; i < this.N; i++) {
+                    this.Rotator.attach(this.StateMap[adj][iterator.index])
+                    iterator.index += iterator.step;
+                }
+    
+                this.#showPlanesAroundLayer(face, index);
+                this.Rotator.attach(this.PlanesBetweenLayers[face][index]);
+            });
+        }
+    
+        const rad = THREE.MathUtils.degToRad(deg * (index === this.N - 1 ? 1 : -1));
+        const axis = this.KERNEL[face].axis;
+        this.Rotator.rotation[axis] = rad;
+        layerRotation[axis] = rad;
+    
+        this.LastRotatedLayer.face = face;
+        this.LastRotatedLayer.index = index;
+        this.LastRotatedLayer.angle = deg;
+    }
+
+
+    #transpose(arr) {
+        const n = Math.round(Math.sqrt(arr.length));
+    
+        for (let i = 0; i < n; i++) {
+            for (let j = i + 1; j < n; j++) {
+                const tmp = arr[i * n + j];
+                arr[i * n + j] = arr[j * n + i];
+                arr[j * n + i] = tmp;
+            }
+        }
+    }
+    
+
+    #invertRows(arr) {
+        const n = Math.round(Math.sqrt(arr.length));
+    
+        for (let k = 0; k < n; k++) {
+            for (let i = n * k, j = i + n - 1; i < j; i++, j--) {
+                const tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
+    
+
+    #swapFaceSlices(faceA, faceB, startIndexA, startIndexB) {
+        const n = Math.round(Math.sqrt(faceA.length));
+    
+        for (let i = 0; i < n; i++) {
+            const tmp = faceA[startIndexA.index];
+            faceA[startIndexA.index] = faceB[startIndexB.index];
+            faceB[startIndexB.index] = tmp;
+    
+            startIndexA.index += startIndexA.step;
+            startIndexB.index += startIndexB.step;
+        }
+    }
+
+
+    #moveLayerSlicesRight(face, index) {
+        const adj = this.KERNEL[face].adjacentFaces;
+        for (let i = 0; i < adj.length - 1; i++) {
+            this.#swapFaceSlices(
+                this.StateMap[adj[i]], 
+                this.StateMap[adj[i + 1]], 
+                this.KERNEL[face][adj[i]](index), 
+                this.KERNEL[face][adj[i + 1]](index));
+        }
+    }
+    
+
+    #moveLayerSlicesLeft(face, index) {
+        const adj = this.KERNEL[face].adjacentFaces;
+        for (let i = adj.length - 1; i > 0; i--) {
+            this.#swapFaceSlices(
+                this.StateMap[adj[i]], 
+                this.StateMap[adj[i - 1]], 
+                this.KERNEL[face][adj[i]](index), 
+                this.KERNEL[face][adj[i - 1]](index));
+        }
+    }
+
+
+    #saveChange(direction, countOfRotations) {
+
+        countOfRotations %= 4;
+
+        const { face, index } = this.LastRotatedLayer;
+
+        while (countOfRotations-- > 0) {
+            if (direction === 'right') {
+                if (index === 0) {
+                    this.#transpose(this.StateMap[face]);
+                    this.#invertRows(this.StateMap[face]);
+                }
+                else if (index === this.N - 1) {
+                    this.#transpose(this.StateMap[this.KERNEL[face].oppositeFace]);
+                    this.#invertRows(this.StateMap[this.KERNEL[face].oppositeFace]);
+    
+                    this.#moveLayerSlicesLeft(face, index);
+                    continue;
+                }
+        
+                this.#moveLayerSlicesRight(face, index);
+            }
+            else if (direction === 'left') {
+                if (index === 0) {
+                    this.#invertRows(this.StateMap[face]);
+                    this.#transpose(this.StateMap[face]);
+                }
+                else if (index === this.N - 1) {
+                    this.#invertRows(this.StateMap[this.KERNEL[face].oppositeFace]);
+                    this.#transpose(this.StateMap[this.KERNEL[face].oppositeFace]);
+    
+                    this.#moveLayerSlicesRight(face, index);
+                    continue;
+                }
+                
+                this.#moveLayerSlicesLeft(face, index);
+            }
+        }
+    }
+
+
+    fixChange() {
+        const { face, index, angle } = this.LastRotatedLayer;
+        const previousAngle = this.KERNEL[face].layerPreviousAngles[index];
+        const dangle = angle - previousAngle;
+    
+        console.log('angle: ' + dangle);
+    
+        if (Math.abs(dangle) % 90 !== 0) {
+            console.log('direction: ' + (dangle < 0 ? 'left' : dangle > 0 ? 'right' : 'no change'));
+            return;
+        }
+    
+        const countOfRotations = Math.abs(dangle) / 90;
+        const direction = dangle < 0 ? 'left' : dangle > 0 ? 'right' : 'no change';
+    
+        console.log('made: ' + countOfRotations);
+        console.log('direction: ' + direction);
+        console.log('\n');
+    
+        this.#hidePlanesAroundLayer(face, index);
+        while (this.Rotator.children.length > 0) {
+            this.ThreeObject.attach(this.Rotator.children[0]);
+        }
+    
+        if (direction !== 'no change') {
+            this.#saveChange(direction, countOfRotations);
+        }
+    
+        this.KERNEL[face].layerPreviousAngles[index] = angle;
+    }
+}
---
+++ public/scripts/CubeTouchController.js
@@ -0,0 +1,299 @@
+import * as THREE from './three_data/build/three.module.js';
+
+export class CubeTouchController {
+
+    constructor(cube, canvas, camera) {
+        this.Raycaster = new THREE.Raycaster();
+        this.Mouse = new THREE.Vector2();
+        this.Camera = camera;
+        
+        this.Canvas = canvas;
+        this.CoordPlane = this.#createCoordinatePlane(100);
+        this.Cube = cube;
+
+        this.CanCompleteLayerRotation = false;
+        this.WasLayerRotatedByMouse = false;
+
+        this.RotCtrl = {
+            face         : '',
+            indexOfPlane : -1,
+            direction    : '',
+            counter      : 0
+        }
+
+        this.TargetAngle = {
+            angle        : 0,
+            countOfSteps : 0,
+            step         : 0
+        }
+
+        this.Canvas.addEventListener('mousedown', (event) => {
+            this.#onMouseDown(event);
+        }, false);
+        
+        // it is neccessary to add/remove the event
+        this.mouseMoveBind = (event) => this.#onMouseMove(event);
+        this.Canvas.addEventListener('mousemove', this.mouseMoveBind, false);
+
+        this.Canvas.addEventListener('mouseup', (event) => {
+            this.#onMouseUp(event);
+        }, false);
+    }
+
+
+    #createCoordinatePlane(size) {
+        const geometry = new THREE.PlaneGeometry(size, size);
+        const material = new THREE.MeshBasicMaterial({
+            side: THREE.FrontSide,
+            polygonOffset: true,
+            polygonOffsetFactor: 0.5,
+            transparent: true,
+            opacity: 0
+        });
+    
+        const plane = new THREE.Mesh(geometry, material);
+        return plane;
+    }
+
+
+    #setCoordinatePlane(face, object, intersectionPoint) {
+        if (this.CoordPlane) {
+            this.Cube.ThreeObject.add(this.CoordPlane);
+            this.CoordPlane.position.set(0, 0, 0);
+            this.CoordPlane.rotation.set(0, 0, 0);
+    
+            object.attach(this.CoordPlane);
+            this.CoordPlane.position.x = intersectionPoint.x;
+            this.CoordPlane.position.y = intersectionPoint.y;
+            this.CoordPlane.position.z = intersectionPoint.z;
+    
+            switch (face) {
+                case 'U':
+                    this.CoordPlane.rotateX(-Math.PI / 2);
+                    break;
+    
+                case 'D':
+                    this.CoordPlane.rotateX(Math.PI / 2);
+                    break;
+    
+                case 'R':
+                    this.CoordPlane.rotateY(Math.PI / 2);
+                    break;
+    
+                case 'L':
+                    this.CoordPlane.rotateY(-Math.PI / 2);
+                    break;
+    
+                case 'F':
+                    break;
+    
+                case 'B':
+                    this.CoordPlane.rotateY(Math.PI);
+                    break;
+            }
+    
+            // coordinate plane should be in cube local space in order not 
+            // to move while rotating a layer
+            this.Cube.ThreeObject.attach(this.CoordPlane);
+        }
+    }
+
+
+    #setMouseCoordinates(event) {
+        this.Mouse.x = (event.clientX / this.Canvas.width) * 2 - 1;
+        this.Mouse.y = -(event.clientY / this.Canvas.height) * 2 + 1;
+    }
+
+
+    #getDirection(x, y) {
+        if (Math.abs(x) <= Math.abs(y)) return 'Y';
+        if (Math.abs(x) >= Math.abs(y)) return 'X';
+    }
+
+
+    onStartTouching() {}
+
+
+    // FIXME: should be called if the cube was touched
+    onStopTouching() {}
+
+
+    #onMouseDown(event) {
+        console.log('mouesdown');
+        this.#setMouseCoordinates(event);
+
+        if (this.CanCompleteLayerRotation === false) {
+            this.Raycaster.setFromCamera(this.Mouse, this.Camera);
+            // TODO: fix cube . . .
+            const intersects = this.Raycaster.intersectObjects(this.Cube.ThreeObject.children);
+        
+            if (intersects.length > 0) {
+                const obj = intersects[0].object;
+                const point = obj.worldToLocal(intersects[0].point);
+                
+                for (let face in this.Cube.StateMap) {
+                    
+                    const index = this.Cube.StateMap[face].indexOf(obj);
+        
+                    if (index > -1) {
+                        console.log('face: ' + face + ' index: ' + index);
+                        this.#setCoordinatePlane(face, obj, point);
+        
+                        this.RotCtrl.indexOfPlane = index;
+                        this.RotCtrl.face = face;
+        
+                        this.Canvas.addEventListener('mousemove', this.mouseMoveBind, false);
+                        break;
+                    }
+                }
+
+                this.onStartTouching();
+            }
+        }
+    }
+
+
+    #onMouseUp(event) {
+        console.log('mouseup');
+        this.Canvas.removeEventListener('mousemove', this.mouseMoveBind);
+        
+        if (this.WasLayerRotatedByMouse) {  
+            this.#setTargetAngle(3);
+            this.WasLayerRotatedByMouse = false;
+            this.CanCompleteLayerRotation = true;
+        }
+        
+        if (this.CoordPlane.parent) {
+            this.CoordPlane.parent.remove(this.CoordPlane);
+        }
+        
+        this.RotCtrl.counter = 0;
+        
+        this.onStopTouching();
+    }
+
+
+    // FIXME: make layer movement more smooth
+    #onMouseMove(event) {
+        this.#setMouseCoordinates(event);
+
+        this.Raycaster.setFromCamera(this.Mouse, this.Camera);
+        const intersects = this.Raycaster.intersectObject(this.CoordPlane);
+
+        if (intersects.length > 0) {
+            
+            const point = this.CoordPlane.worldToLocal(intersects[0].point);
+
+            if (this.RotCtrl.counter < 5) {
+                if (Math.abs(point.x) > 0 || Math.abs(point.y) > 0) {
+                    this.RotCtrl.counter++;
+                    this.RotCtrl.direction = this.#getDirection(point.x, point.y);
+                }
+            }
+            else {
+                const row = Math.floor(this.RotCtrl.indexOfPlane / this.Cube.N);
+                const col = this.RotCtrl.indexOfPlane % this.Cube.N;
+                const factor = 20;
+
+                const faceTouchInfo = this.#getFaceTouchInfo(row, col);
+
+                if (this.RotCtrl.face) {
+                    const deg = (this.RotCtrl.direction === 'X' ? point.x : point.y) * factor;
+                    const info = faceTouchInfo[this.RotCtrl.face][this.RotCtrl.direction];
+
+                    // TODO: hide KERNEL . . .
+                    const angle = this.Cube.KERNEL[info.face].layerPreviousAngles[info.indexOfLayer];
+
+                    this.Cube.rotateLayer(
+                        info.face, 
+                        info.indexOfLayer, 
+                        angle + (info.isOppositeLayer ? deg : -deg));
+
+                    this.WasLayerRotatedByMouse = true;
+                }
+            }
+        }
+    }
+
+
+    #getFaceTouchInfo(row, col) {
+        const N = this.Cube.N;
+        return {
+            F : {
+                X : { face : 'U', indexOfLayer : row,         isOppositeLayer : row === N - 1 },
+                Y : { face : 'R', indexOfLayer : N - col - 1, isOppositeLayer : col !== 0 }
+            },
+        
+            B : {
+                X : { face : 'U', indexOfLayer : row,         isOppositeLayer : row === N - 1 },
+                Y : { face : 'R', indexOfLayer : col,         isOppositeLayer : col === N - 1 }
+            },
+        
+            U : {
+                X : { face : 'F', indexOfLayer : N - row - 1, isOppositeLayer : row !== 0 },
+                Y : { face : 'R', indexOfLayer : N - col - 1, isOppositeLayer : col !== 0 }
+            },
+        
+            D : {
+                X : { face : 'F', indexOfLayer : row,         isOppositeLayer : row === N - 1 },
+                Y : { face : 'R', indexOfLayer : N - col - 1, isOppositeLayer : col !== 0 }
+            },
+        
+            R : {
+                X : { face : 'U', indexOfLayer : row,         isOppositeLayer : row === N - 1 },
+                Y : { face : 'F', indexOfLayer : col,         isOppositeLayer : col === N - 1 }
+            },
+        
+            L : {
+                X : { face : 'U', indexOfLayer : row,         isOppositeLayer : row === N - 1 },
+                Y : { face : 'F', indexOfLayer : N - col - 1, isOppositeLayer : col !== 0 }
+            }
+        };
+    }
+
+
+    #setTargetAngle(positiveStep) {
+        const angle = this.Cube.LastRotatedLayer.angle;
+        const sign =  angle >= 0 ? 1 : -1;
+        const alpha = Math.abs(angle);
+    
+        const leftTarget = 90 * Math.floor(alpha / 90);
+        const rightTarget = 90 * Math.ceil(alpha / 90);
+    
+        const leftDelta = alpha - leftTarget;
+        const rightDelta = rightTarget - alpha;
+    
+        if (leftDelta <= rightDelta) {
+            this.TargetAngle.angle = leftTarget * sign;
+            this.TargetAngle.countOfSteps = Math.floor(leftDelta / positiveStep);
+            this.TargetAngle.step = positiveStep * -sign;
+        }
+        else {
+            this.TargetAngle.angle = rightTarget * sign;
+            this.TargetAngle.countOfSteps = Math.floor(rightDelta / positiveStep);
+            this.TargetAngle.step = positiveStep * sign;
+        }
+    }
+
+
+    #completeLayerRotation() {
+        const { face, index, angle } = this.Cube.LastRotatedLayer;
+
+        if (this.TargetAngle.countOfSteps > 0) {
+            this.TargetAngle.countOfSteps--;
+            this.Cube.rotateLayer(face, index, angle + this.TargetAngle.step);
+        }
+        else {
+            this.Cube.rotateLayer(face, index, this.TargetAngle.angle);
+            this.Cube.fixChange();
+            this.CanCompleteLayerRotation = false;
+        }
+    }
+
+
+    update() {
+        if (this.CanCompleteLayerRotation) {
+            this.#completeLayerRotation();
+        }
+    }
+}
