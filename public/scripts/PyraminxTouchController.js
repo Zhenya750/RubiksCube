95cad842aa12c4b79d2f01fc3ee8d38e2d9d10f8
zhenya750 <zhenya750bus@mail.ru>
2021-02-04 21:01:02 +0300

    rotating pyraminx layers using coordinate plane

--- public/scripts/PyraminxTouchController.js
+++ public/scripts/PyraminxTouchController.js
@@ -2,13 +2,11 @@ import * as THREE from './three_data/build/three.module.js';
 
 export const PyraminxTouchController = function(pyraminx, canvas, camera) {
 
-    this.enabled = true;
-
     const raycaster = new THREE.Raycaster();
     const mouse = new THREE.Vector2();
     const coordPlane = createCoordinatePlane(10);
+    let touchInfo;
 
-    
     // private methods
     function setCenteredMouseCoordinates(event) {
         mouse.x = (event.clientX / canvas.width) * 2 - 1;
@@ -16,7 +14,7 @@ export const PyraminxTouchController = function(pyraminx, canvas, camera) {
     }
     
 
-    function onMouseDown(event) {
+    const onMouseDown = (event) => {
 
         if (this.enabled === false) return;
 
@@ -31,23 +29,94 @@ export const PyraminxTouchController = function(pyraminx, canvas, camera) {
 
             const triangle = intersects[0].object;
             const point = triangle.worldToLocal(intersects[0].point);
-            const info = pyraminx.getTriangleInfo(triangle);
+            touchInfo = pyraminx.getTriangleInfo(triangle);
 
-            if (info.face === '') return;
+            if (touchInfo.face === '') return;
 
-            setCoordinatePlane(triangle, point, info);
+            setCoordinatePlane(triangle, point, touchInfo);
+
+            canvas.addEventListener('mousemove', onMouseMove, false);
+
+            this.onStartTouching();
         }
     }
 
+    const flag = new THREE.AxesHelper(0.5);
 
-    function onMouseUp(event) {
+    const rotationState = {
+        state : 'PREPARING',
+        layer : null,
+        previousDistance : 0,
+        direction : null
+    };
+
+
+    const onMouseUp = (event) => {
 
         if (this.enabled === false) return;
 
         console.log('mouse up');
 
+        rotationState.state = 'PREPARING';
+        rotationState.layer = null;
+        rotationState.previousDistance = 0;
+        rotationState.direction = null;
+
         if (coordPlane.parent) {
             coordPlane.parent.remove(coordPlane);
+        }
+
+        canvas.removeEventListener('mousemove', onMouseMove);
+
+        this.onStopTouching();
+    }
+
+
+    const onMouseMove = (event) => {
+
+        if (this.enabled === false) return;
+        if (!coordPlane) return;
+
+        setCenteredMouseCoordinates(event, mouse);
+
+        raycaster.setFromCamera(mouse, camera);
+        const intersects = raycaster.intersectObject(coordPlane);
+
+        if (intersects.length > 0) {
+            
+            let point = coordPlane.worldToLocal(intersects[0].point);
+            let distance = point.distanceTo(new THREE.Vector3(0, 0, 0));
+
+            switch (rotationState.state) {
+
+                case 'PREPARING':
+                    if (distance > 0.02) {
+                        rotationState.state = 'DEFINING';
+                    }
+                    break;
+
+                case 'DEFINING':
+                    rotationState.direction = getDirection(point.x, point.y);
+                    rotationState.layer = getLayer(touchInfo, rotationState.direction.name);
+    
+                    rotationState.state = 'ROTATING';
+                    break;
+
+                case 'ROTATING':
+                    let project = point.projectOnVector(rotationState.direction.vector);
+                    let len = project.length();
+    
+                    coordPlane.add(flag);
+                    flag.position.set(project.x, project.y, 0);
+    
+                    const sign = project.dot(rotationState.direction.vector) >= 0 ? 1 : -1;
+                    const delta = len - rotationState.previousDistance;
+
+                    pyraminx.rotateLayer(rotationState.layer, sign * delta * this.speedFactor);
+
+                    rotationState.previousDistance = len;
+                    break;
+            }
         }
     }
 
@@ -86,8 +155,71 @@ export const PyraminxTouchController = function(pyraminx, canvas, camera) {
 
     canvas.addEventListener('mousedown', onMouseDown, false);
     canvas.addEventListener('mouseup', onMouseUp, false);
+
+
+    // public interface
+    this.enabled = true;
+
+    this.speedFactor = pyraminx.dimension * 7;
+
+
+    this.onStartTouching = function() {}
+
+
+    this.onStopTouching = function() {}
 }
 
+
+function getDirection(x, y) {
+
+    const k = Math.sqrt(3);
+
+    const direction = {
+        name : 'H',
+        vector: new THREE.Vector3(0, 0, 0)
+    }
+
+    // !!! vector numbers matter !!!
+    // they affect the direction of rotation
+    if (1 / k * Math.abs(x) >= Math.abs(y)) {
+        direction.name = 'H';
+        direction.vector.set(1, 0, 0);
+    }
+
+    if ((1 / k * x <= y && x >= 0) || (1 / k * x >= y && x <= 0)) {
+        direction.name = 'L';
+        direction.vector.set(-1, -k, 0);
+    }
+
+    if ((-1 / k * x <= y && x <= 0) || (-1 / k * x >= y && x >= 0)) {
+        direction.name = 'R';
+        direction.vector.set(-1, k, 0);
+    }
+
+    return direction;
+}
+
+
+function getLayer(touchInfo, directionName) {
+
+    const directionToLayerIndex = {
+        R : touchInfo.rightDiagonal,
+        L : touchInfo.leftDiagonal,
+        H : touchInfo.horizontal
+    };
+
+    const directionToFace = {
+        F : { R : 'R', L : 'L', H : 'D' },
+        R : { R : 'L', L : 'F', H : 'D' },
+        L : { R : 'F', L : 'R', H : 'D' },
+        D : { R : 'L', L : 'R', H : 'F' }
+    };
+
+    return {
+        face : directionToFace[touchInfo.face][directionName],
+        index : directionToLayerIndex[directionName]
+    };
+}
 
 function createCoordinatePlane(size) {
     const geometry = new THREE.PlaneGeometry(size, size);
